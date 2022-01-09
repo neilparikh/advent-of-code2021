addcbbcad0eacfcb734b3749fe6a07aec441dc1b
Neil Parikh <n3parikh@csclub.uwaterloo.ca>
2022-01-09 08:23:29 -0500

    speed up day 12

--- day12.hs
+++ day12.hs
@@ -7,18 +7,33 @@ import Data.List (sortOn, nub)
 
 main :: IO ()
 main = do
-  input <- fmap (fmap (listToTuple . wordsOn '-') . lines) (readFile "day12.in")
+  input <- fmap (fmap (listToTuple . fmap stringToNode . wordsOn '-') . lines) (readFile "day12.in")
   let input' = M.fromList . fmap (\ls -> (fst . head $ ls, fmap snd ls)) . groupOn fst . sortOn fst . nub $ input ++ fmap swap input
-  print $ go input' S.empty True "start"
-  print $ go input' S.empty False "start"
+  print $ go input' S.empty True Start
+  print $ go input' S.empty False Start
   return ()
 
-go :: M.Map String [String] -> S.Set String -> Bool -> String -> Int
-go m s dup "end" = 1
-go m s dup x 
-  | all isLower x && x `S.member` s && (dup || x == "start") = 0
+data Node = Lower Char | Upper Char | Start | End
+  deriving (Eq, Ord, Show)
+
+stringToNode :: String -> Node
+stringToNode "start" = Start
+stringToNode "end" = End
+stringToNode s | isLower (head s) = Lower (head s)
+               | otherwise = Upper (head s)
+
+go :: M.Map Node [Node] -> S.Set Node -> Bool -> Node -> Int
+go m s dup End = 1
+go m s dup Start | Start `S.member` s = 0
+go m s dup Start = let
+  s' = S.insert Start s
+  newNodes = m M.! Start
+  in sum . map (go m s' dup) $ newNodes
+go m s dup x@(Upper _) = let newNodes = m M.! x in sum . map (go m s dup) $ newNodes
+go m s dup x@(Lower _)
+  | x `S.member` s && dup = 0
   | otherwise = let
     s' = S.insert x s
     newNodes = m M.! x
-    dup' = dup || (x `S.member` s && all isLower x)
+    dup' = dup || x `S.member` s
     in sum . map (go m s' dup') $ newNodes
