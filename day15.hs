15c1948909cd13d6f877826e33921e34673d266e
Neil Parikh <parikh.neil@me.com>
2022-11-02 23:57:24 -0600

    "monadify"

--- day15.hs
+++ day15.hs
@@ -1,20 +1,23 @@
 -- import Data.Map.String as M
 import Util (safeAt)
 import Data.Maybe (catMaybes)
+import Control.Monad.Identity
 
 main :: IO ()
 main = do
   input <- parseInput <$> getContents
-  print $ subtract (input !! 0 !! 0) <$> part1 input (0, 0)
+  print $ subtract (input !! 0 !! 0) <$> (runIdentity $ part1 input (0, 0))
   return ()
 
 parseInput :: String -> [[Int]]
 parseInput = fmap (fmap (read . (: []))) . lines
 
-part1 :: [[Int]] -> (Int, Int) -> Maybe Int
+part1 :: (Monad m) => [[Int]] -> (Int, Int) -> m (Maybe Int)
 part1 map (x, y)
-  | x >= (length (map !! 0)) || y >= length map = Nothing
-  | otherwise = Just $ ((map !! y) !! x) + nextCost
-  where
-  next = catMaybes [part1 map (x + 1, y), part1 map (x, y + 1)]
-  nextCost = if (length next > 0) then minimum next else 0
+  | x >= (length (map !! 0)) || y >= length map = pure Nothing
+  | otherwise = do
+      r <- part1 map (x + 1, y)
+      d <- part1 map (x, y + 1)
+      let next = catMaybes [r, d]
+      let nextCost = if (length next > 0) then minimum next else 0
+      pure $ Just $ ((map !! y) !! x) + nextCost
